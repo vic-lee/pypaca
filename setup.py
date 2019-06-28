047e36aa6a5108b35e30773d08d6188ad88ad40f
vic-lee <viclee@nyu.edu>
2019-06-28 12:37:26 -0400

    Initialize setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pypaca',
+      version='0.1',
+      description='Simple utilities for complex projects.',
+      url='https://github.com/vic-lee/pypaca',
+      author='Vic Lee',
+      author_email='viclee@nyu.edu',
+      license='MIT',
+      packages=['pypaca'],
+      zip_safe=False)
