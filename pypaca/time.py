6ab78b004c3242cc3517d4ac33c4efa8a68e6967
vic-lee <viclee@nyu.edu>
2019-06-28 16:21:29 -0400

    Add time-related functions

---
+++ pypaca/time.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+
+def timer(func):
+    """A decorator that times and prints execution time."""
+    def timer_wrapper(*args, **kwargs):
+        start_time = datetime.now()
+        resp = func(*args, **kwargs)
+        end_time = datetime.now()
+        duration = end_time - start_time
+        print("{:<40} runtime: {}.{} secs".format(
+            func.__name__, duration.seconds, duration.microseconds))
+        return resp
+    return timer_wrapper
+
+
+def timeout(delay, ExceptionType=Exception):
+    def _timeout(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            start = datetime.now()
+            while True:
+                try:
+                    ret = func(self, *args, **kwargs)
+                except ExceptionType:
+                    continue
+                finally:
+                    now = datetime.now()
+                    if (now - start).seconds > delay:
+                        raise TimeoutError
+                        break
+            return ret
+        return wrapper
+    return _timeout
