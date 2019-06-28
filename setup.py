6394f285bb0ad498f7257bb834608bb151b47ab8
vic-lee <viclee@nyu.edu>
2019-06-28 17:19:00 -0400

    Update setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(name='pypaca',
-      version='0.1',
-      description='Simple utilities for complex projects.',
-      url='https://github.com/vic-lee/pypaca',
-      author='Vic Lee',
-      author_email='viclee@nyu.edu',
-      license='MIT',
-      packages=['pypaca'],
-      zip_safe=False)
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pypaca",
+    version="0.0.2",
+    author="Vic Lee",
+    author_email="viclee@nyu.edu",
+    description="Simple utilities for complex projects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vic-lee/pypaca",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
